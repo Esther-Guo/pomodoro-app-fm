f15724271a4b75c816498b23655bd495659e5cac
Esther Guo <esther_guo@qq.com>
2022-01-15 23:36:59 +0800

    project structure demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from "./images/logo.svg";
+import settingsIcon from "./images/icon-settings.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="title" src={Logo} alt="pomodoro logo" />
+      <div className="control-panel">
+        <Button />
+        <Button />
+        <Button />
+      </div>
+      <Clock />
+      <Settings />
+      <img src={settingsIcon} alt="settings icon" className="settingsIcon" />
     </div>
   );
 }
